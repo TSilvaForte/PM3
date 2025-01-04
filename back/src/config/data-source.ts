b272ab333e74205c5c6780405949175e793eef2d
TSilvaForte <tamara.sfok@gmail.com>
2025-01-04 18:59:38 -0300

    Adding render db

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -1,4 +1,4 @@
-import { DataSource } from "typeorm";
+/* import { DataSource } from "typeorm";
 
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from "./envs";
 import { User } from "../entities/User";
@@ -22,4 +22,30 @@ export const AppDataSource = new DataSource({
 
 export const userModel = AppDataSource.getRepository(User)
 export const credentialModel = AppDataSource.getRepository(Credential)
-export const appointmentModel = AppDataSource.getRepository(Appointment)
+export const appointmentModel = AppDataSource.getRepository(Appointment) */
+
+import { DataSource } from "typeorm";
+import { DB_URL } from "./envs";
+import { User } from "../entities/User";
+import { Credential } from "../entities/Credential";
+import { Appointment } from "../entities/Appointment";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    url: DB_URL, 
+    synchronize: true,
+    dropSchema: false,
+    logging: false,
+    entities: [User, Credential, Appointment],
+    subscribers: [],
+    migrations: [],
+    extra: {
+        ssl: {
+            rejectUnauthorized: false, 
+        },
+    },
+});
+
+export const userModel = AppDataSource.getRepository(User);
+export const credentialModel = AppDataSource.getRepository(Credential);
+export const appointmentModel = AppDataSource.getRepository(Appointment);
--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,8 +1,21 @@
-import "dotenv/config";
+/* import "dotenv/config";
 
 export const PORT = process.env.PORT;
 export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = Number(process.env.DB_PORT);
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_NAME = process.env.DB_NAME;
+export const DB_NAME = process.env.DB_NAME; */
+
+import "dotenv/config";
+
+const requiredEnvs = ["PORT", "DB_URL"];
+
+requiredEnvs.forEach((key) => {
+    if (!process.env[key]) {
+        throw new Error(`Missing required environment variable: ${key}`);
+    }
+});
+
+export const PORT = process.env.PORT!;
+export const DB_URL = process.env.DB_URL!;
