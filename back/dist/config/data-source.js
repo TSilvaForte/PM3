2aa53a03789465c3cdf3615d935ceb40db2e4057
TSilvaForte <tamara.sfok@gmail.com>
2025-01-18 22:28:28 -0300

    Changing start command

--- back/dist/config/data-source.js
+++ back/dist/config/data-source.js
@@ -1,4 +1,29 @@
 "use strict";
+/* import { DataSource } from "typeorm";
+
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from "./envs";
+import { User } from "../entities/User";
+import { Credential } from "../entities/Credential";
+import { Appointment } from "../entities/Appointment";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    synchronize: true,
+    dropSchema: false,
+    logging: false,
+    entities: [User, Credential, Appointment],
+    subscribers: [],
+    migrations: [],
+})
+
+export const userModel = AppDataSource.getRepository(User)
+export const credentialModel = AppDataSource.getRepository(Credential)
+export const appointmentModel = AppDataSource.getRepository(Appointment) */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.appointmentModel = exports.credentialModel = exports.userModel = exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
@@ -8,17 +33,18 @@ const Credential_1 = require("../entities/Credential");
 const Appointment_1 = require("../entities/Appointment");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: envs_1.DB_HOST,
-    port: envs_1.DB_PORT,
-    username: envs_1.DB_USERNAME,
-    password: envs_1.DB_PASSWORD,
-    database: envs_1.DB_NAME,
+    url: envs_1.DB_URL,
     synchronize: true,
     dropSchema: false,
     logging: false,
     entities: [User_1.User, Credential_1.Credential, Appointment_1.Appointment],
     subscribers: [],
     migrations: [],
+    extra: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
 });
 exports.userModel = exports.AppDataSource.getRepository(User_1.User);
 exports.credentialModel = exports.AppDataSource.getRepository(Credential_1.Credential);
--- back/dist/config/envs.js
+++ back/dist/config/envs.js
@@ -1,10 +1,20 @@
 "use strict";
+/* import "dotenv/config";
+
+export const PORT = process.env.PORT;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PORT = Number(process.env.DB_PORT);
+export const DB_USERNAME = process.env.DB_USERNAME;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME; */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_NAME = exports.DB_PASSWORD = exports.DB_USERNAME = exports.DB_PORT = exports.DB_HOST = exports.PORT = void 0;
+exports.DB_URL = exports.PORT = void 0;
 require("dotenv/config");
+const requiredEnvs = ["PORT", "DB_URL"];
+requiredEnvs.forEach((key) => {
+    if (!process.env[key]) {
+        throw new Error(`Missing required environment variable: ${key}`);
+    }
+});
 exports.PORT = process.env.PORT;
-exports.DB_HOST = process.env.DB_HOST;
-exports.DB_PORT = Number(process.env.DB_PORT);
-exports.DB_USERNAME = process.env.DB_USERNAME;
-exports.DB_PASSWORD = process.env.DB_PASSWORD;
-exports.DB_NAME = process.env.DB_NAME;
+exports.DB_URL = process.env.DB_URL;
